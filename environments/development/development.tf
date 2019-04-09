fd1302c24ecb7a51705ae4f61c3b856384ce41aa
Taku Ito <t.itou1201@gmail.com>
2019-04-09 23:12:00 +0900

    ✨ DHCPの設定を追加 (#3)
    * dhcpを追加
    * cidr_blockが重複していたので削除

--- environments/development/development.tf
+++ environments/development/development.tf
@@ -1,6 +1,7 @@
 variable "name" {}
 variable "environment" {}
 variable "region" {}
+variable "domain_name" {}
 variable "cidr_block" {}
 variable "cidr_block_public_1a" {}
 variable "cidr_block_public_1c" {}
@@ -16,6 +17,7 @@ module "network" {
     name                  = "${var.name}"
     environment           = "${var.environment}"
     region                = "${var.region}"
+    domain_name           = "${var.domain_name}"
     cidr_block            = "${var.cidr_block}"
     cidr_block_public_1a  = "${var.cidr_block_public_1a}"
     cidr_block_public_1c  = "${var.cidr_block_public_1c}"
