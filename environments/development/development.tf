e309fa4489ef4663d1ccbd3c381e062dcda237a3
Taku Ito <t.itou1201@gmail.com>
2019-04-09 22:20:23 +0900

    ✨ subnetsを追加 (#1)

--- environments/development/development.tf
+++ environments/development/development.tf
@@ -1,15 +1,24 @@
 variable "name" {}
 variable "environment" {}
+variable "region" {}
 variable "cidr_block" {}
+variable "cidr_block_public_1a" {}
+variable "cidr_block_public_1c" {}
+variable "cidr_block_private_1a" {}
+variable "cidr_block_private_1c" {}
 
 provider "aws" {
     region = "ap-northeast-1"
 }
 
-
 module "network" {
-    source      = "../../modules/network"
-    name        = "${var.name}"
-    environment = "${var.environment}"
-    cidr_block  = "${var.cidr_block}"
+    source                = "../../modules/network"
+    name                  = "${var.name}"
+    environment           = "${var.environment}"
+    region                = "${var.region}"
+    cidr_block            = "${var.cidr_block}"
+    cidr_block_public_1a  = "${var.cidr_block_public_1a}"
+    cidr_block_public_1c  = "${var.cidr_block_public_1c}"
+    cidr_block_private_1a = "${var.cidr_block_private_1a}"
+    cidr_block_private_1c = "${var.cidr_block_private_1c}"
 }
--- modules/network/network.tf
+++ modules/network/network.tf
@@ -1,12 +1,29 @@
 variable "name" {}
 variable "environment" {}
+variable "region" {}
 variable "cidr_block" {}
+variable "cidr_block_public_1a" {}
+variable "cidr_block_public_1c" {}
+variable "cidr_block_private_1a" {}
+variable "cidr_block_private_1c" {}
 
 module "vpc" {
     source      = "./vpc"
     name        = "${var.name}"
     environment = "${var.environment}"
     cidr_block  = "${var.cidr_block}"
+}
+
+module "subnets" {
+    source                = "./subnets"
+    name                  = "${var.name}"
+    environment           = "${var.environment}"
+    region                = "${var.region}"
+    vpc_id                = "${module.vpc.vpc_id}"
+    cidr_block_public_1a  = "${var.cidr_block_public_1a}"
+    cidr_block_public_1c  = "${var.cidr_block_public_1c}"
+    cidr_block_private_1a = "${var.cidr_block_private_1a}"
+    cidr_block_private_1c = "${var.cidr_block_private_1c}"
 }
 
 output "vpc_id" {
